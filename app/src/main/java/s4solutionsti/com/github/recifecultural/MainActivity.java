20c6ec855984fe39da13e99f82bf6f70c6aded63
José Francisco da Silva IV <silvaivctd@gmail.com>
2016-12-16 01:01:48 -0300

    usando o Adapter para adicionar dados a um ListView

--- app/src/main/java/s4solutionsti/com/github/recifecultural/MainActivity.java
+++ app/src/main/java/s4solutionsti/com/github/recifecultural/MainActivity.java
@@ -2,6 +2,7 @@ package s4solutionsti.com.github.recifecultural;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
         String [] pontos= {"Recife Antigo","Marco Zero", "Fonte das Cinco Pontas", "Porto de Galinha"};
         ListView listaPontos = (ListView) findViewById(R.id.lista_pontos);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter <String> (this, android.R.layout.simple_list_item_1, pontos);
+        listaPontos.setAdapter(adapter);
     }
 }
